a321871166109e3431f6273043e92076067bf282
Zachary Dremann <dremann@gmail.com>
2015-10-13 19:53:38 -0400

    splice: add wrapper functions for squash_splice_custom*

--- src/splice.rs
+++ src/splice.rs
@@ -33,6 +33,30 @@ extern {
         fp_in: *mut FILE,
         size: size_t,
         options: *mut SquashOptions) -> SquashStatus;
+    pub fn squash_splice_custom(
+        codec: *const c_char,
+        stream_type: SquashStreamType,
+        write_cb: SquashWriteFunc,
+        read_cb: SquashReadFunc,
+        user_data: *mut c_void,
+        size: size_t,
+        ...) -> SquashStatus;
+    pub fn squash_splice_custom_codec(
+        codec: *mut SquashCodec,
+        stream_type: SquashStreamType,
+        write_cb: SquashWriteFunc,
+        read_cb: SquashReadFunc,
+        user_data: *mut c_void,
+        size: size_t,
+        ...) -> SquashStatus;
+    pub fn squash_splice_custom_with_options(
+        codec: *const c_char,
+        stream_type: SquashStreamType,
+        write_cb: SquashWriteFunc,
+        read_cb: SquashReadFunc,
+        user_data: *mut c_void,
+        size: size_t,
+        options: *mut SquashOptions) -> SquashStatus;
     pub fn squash_splice_custom_codec_with_options(
         codec: *mut SquashCodec,
         stream_type: SquashStreamType,
