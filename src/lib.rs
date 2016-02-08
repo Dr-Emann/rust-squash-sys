a852f696983160a6d7b149e64ae30337b23c0e70
Zachary Dremann <dremann@gmail.com>
2016-02-07 22:28:00 -0500

    Remove public exports of modules, only export items

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,18 @@ extern crate libc;
 #[macro_use]
 extern crate bitflags;
 
-pub mod object;
-pub mod codec;
-pub mod status;
-pub mod plugin;
-pub mod license;
-pub mod option;
-pub mod stream;
-pub mod version;
-pub mod file;
-pub mod context;
-pub mod splice;
-pub mod memory;
+mod object;
+mod codec;
+mod status;
+mod plugin;
+mod license;
+mod option;
+mod stream;
+mod version;
+mod file;
+mod context;
+mod splice;
+mod memory;
 
 pub use object::*;
 pub use codec::*;
