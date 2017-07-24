2d71d9e3fca5487481d7d49920e51e38756958b7
Zachary Dremann <dremann@gmail.com>
2017-07-23 20:55:37 -0400

    Another travis try

--- ci/script.sh
+++ ci/script.sh
@@ -10,12 +10,21 @@ main() {
     cargo build
     cargo build --release
 
+    echo "$TRAVIS_RUST_VERSION"
+    if [ "$TRAVIS_RUST_VERSION" -eq "nightly" ]; then
+        cargo build --features=nightly
+    fi
+
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
     cargo test
     cargo test --release
+    if [ "$TRAVIS_RUST_VERSION" -eq "nightly" ]; then
+        cargo test --features=nightly
+        cargo test --release --features=nightly
+    fi
 
     # cross test --target $TARGET
     # cross test --target $TARGET --release
