9cdea9bb2523d48586141f95eb752a7660a9042f
Zachary Dremann <dremann@gmail.com>
2019-10-22 14:13:21 -0400

    Update for docs.rs

--- build.rs
+++ build.rs
@@ -2,5 +2,7 @@ extern crate pkg_config;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+
+    #[cfg(not(feature = "docs-rs"))]
     pkg_config::probe_library("squash-0.8").unwrap();
 }
